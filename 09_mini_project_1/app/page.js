6122d78632d3773d2acf631c60cfd669eb71e80a
Yash25032003 <yashsharma210300@acropolis.in>
2026-01-01 18:28:45 +0530

    add mini-project-1 completed

--- 09_mini_project_1/app/page.js
+++ 09_mini_project_1/app/page.js
@@ -1,9 +1,71 @@
 import Image from "next/image";
+import { Heart, Smartphone } from "lucide-react";
 
 export default function Home() {
   return (
-    <div>
-      <h1> Home</h1>
+    <div className="max-w-6xl mx-auto px-4 py-12">
+      {/* Hero section */}
+      <div className="text-center mb-16">
+        <h1 className="tex-4xl md:text-6xl font-bold text-gray-900 mb-6">
+          Welcome to my website
+        </h1>
+        <p className="text-xl text-gray-600 max-w-2xl mx-auto mb-8">
+          This is a simple, clean website built with Next.js and Tailwind CSS.
+          Perfect for beginners learning web development.
+        </p>
+        <div className="space-x-4">
+          <button className="bg-blue-600 text-white px-3 py-3 rounded-lg hover:bg-blue-700 transition-colors font-semibold">
+            Get Started
+          </button>
+
+          <button className="border border-gray-300 text-gray-700 px-6 py-3 rounded-lg hover:bg-gray-50 transition-colors">
+            Learn more
+          </button>
+        </div>
+      </div>
+      {/* Features section */}
+      <div className="grid md:grid-cols-3 gap-8 mb-16">
+        <div className="text-center p-6 bg-whit rounded-lg shadow-sm border border-gray-200 ">
+          <div className="w-12 h-12 bg-blue-100 rounded-lg flex items-center justify-center mx-auto mb-4">
+            <svg
+              className="w-6 h-6 text-blue-600"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M13 10V3L4 14h7v7l9-11h-7z"
+              />
+            </svg>
+          </div>
+          <h3 className="text-xl font-semibold text-gray-900 mb-3"> Fast</h3>
+          <p className="italic text-gray-600">
+            Built With Modern Tech for Optimal Performance
+          </p>
+        </div>
+        <div className="text-center p-6 bg-whit rounded-lg shadow-sm border border-gray-200 ">
+          <div className="w-12 h-12 bg-green-100 rounded-lg flex items-center justify-center mx-auto mb-4">
+            <Heart className="text-green-500" />
+          </div>
+          <h3 className="text-xl font-semibold text-gray-900 mb-3"> Simple</h3>
+          <p className="italic text-gray-600">Clean and Easy to understand</p>
+        </div>
+        <div className="text-center p-6 bg-whit rounded-lg shadow-sm border border-gray-200 ">
+          <div className="w-12 h-12 bg-pink-100 rounded-lg flex items-center justify-center mx-auto mb-4">
+            <Smartphone className="text-pink-500" />
+          </div>
+          <h3 className="text-xl font-semibold text-gray-900 mb-3">
+            {" "}
+            Responsive
+          </h3>
+          <p className="italic text-gray-600">
+            Works Perfectly on all devices and screen sizes
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
